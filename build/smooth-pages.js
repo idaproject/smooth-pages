1dac655525efa7f8d15de748f90ca6cf31cbda81
Arseny Sysolyatin <phelansav@gmail.com>
2016-05-31 18:33:07 +0300

    New parameter spinnerSize

--- build/smooth-pages.js
+++ build/smooth-pages.js
@@ -16,7 +16,8 @@
     var config = {
       overlayColor: '#ffffff',
       fadeOut: true,
-      spinnerImage: 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#808080" d="M17.25 1.5c-.14-.06-.28-.11-.44-.11-.55 0-1 .45-1 1 0 .39.23.72.56.89l-.01.01c3.2 1.6 5.39 4.9 5.39 8.71 0 5.38-4.37 9.75-9.75 9.75S2.25 17.39 2.25 12c0-3.82 2.2-7.11 5.39-8.71v-.02c.33-.16.56-.49.56-.89 0-.55-.45-1-1-1-.16 0-.31.05-.44.11C2.9 3.43.25 7.4.25 12c0 6.49 5.26 11.75 11.75 11.75S23.75 18.49 23.75 12c0-4.6-2.65-8.57-6.5-10.5z"></path></svg>'
+      spinnerImage: 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#808080" d="M17.25 1.5c-.14-.06-.28-.11-.44-.11-.55 0-1 .45-1 1 0 .39.23.72.56.89l-.01.01c3.2 1.6 5.39 4.9 5.39 8.71 0 5.38-4.37 9.75-9.75 9.75S2.25 17.39 2.25 12c0-3.82 2.2-7.11 5.39-8.71v-.02c.33-.16.56-.49.56-.89 0-.55-.45-1-1-1-.16 0-.31.05-.44.11C2.9 3.43.25 7.4.25 12c0 6.49 5.26 11.75 11.75 11.75S23.75 18.49 23.75 12c0-4.6-2.65-8.57-6.5-10.5z"></path></svg>',
+      spinnerSize: 24
     };
     if (options && !options instanceof Object) {
       throw new Error('options parameter must be an object');
@@ -72,7 +73,11 @@
       overlay.appendChild(preloader);
       preloader.setAttribute('src', config.spinnerImage);
       preloader.style.cssText =
-        'position: absolute; top: 50%; left: 50%; margin-top: -12px; margin-left: -12px; ' +
+        'position: absolute; top: 50%; left: 50%;' +
+        'width: ' + config.spinnerSize + 'px;' +
+        'height: ' + config.spinnerSize + 'px;' +
+        'margin-top: -' + config.spinnerSize / 2 + 'px;' +
+        'margin-left: -' + config.spinnerSize / 2 + 'px; ' +
         'animation: preloader-spin 1s linear infinite, preloader-fade-in 0.1s linear;';
       sheet.appendChild(document.createTextNode('body { display: block; }'));
       if (config.fadeOut) {
@@ -81,8 +86,8 @@
     });
 
     window.addEventListener('load', function () {
-      overlay.style.opacity = '0';
-      overlay.style.zIndex = '-1';
+      // overlay.style.opacity = '0';
+      // overlay.style.zIndex = '-1';
       document.documentElement.style.backgroundColor = htmlColor;
     });
   };
